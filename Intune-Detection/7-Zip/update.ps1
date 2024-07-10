c0e0fe238493336fe7b0215c41e579a993f73930
calvin-quint <138726054+calvin-quint@users.noreply.github.com>
2024-07-10 14:36:17 -0500

    Update update.ps1

--- Intune-Detection/7-Zip/update.ps1
+++ Intune-Detection/7-Zip/update.ps1
@@ -8,7 +8,6 @@ $registryPath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
 try {
     $7ZipItems = Get-ItemProperty -Path $registryPath | Where-Object { $_.DisplayName -like '*7-Zip*' }
 } catch {
-    Write-Output "Failed to query the registry. Error: $_"
     exit 1
 }
 
@@ -18,9 +17,7 @@ if ($7ZipItems) {
         # Retrieve the version of the installed 7-Zip
         try {
             $Version = $7Zip.DisplayVersion
-            Write-Output "7-Zip detected. Version: $Version"
         } catch {
-            Write-Output "Error retrieving version for 7-Zip entry. Error: $_"
             continue
         }
 
@@ -28,16 +25,13 @@ if ($7ZipItems) {
         try {
             $7ZipProductCode = $7Zip.ModifyPath -replace '^.*?(\{[A-F0-9\-]+\}).*$', '$1'
         } catch {
-            Write-Output "Failed to extract product code from ModifyPath. Error: $_"
             continue
         }
         
         # Compare the version with the minimum required version
         if ([version]$Version -lt [version]$MinimumVersion) {
-            Write-Output "Uninstalling 7-Zip version $Version..."
-            
-            # Define the uninstallation command with logging
-            $UninstallCommand = "/x $($7ZipProductCode) /qn /l*v `"$env:TEMP\7Zip_Uninstall.log`""
+            # Define the uninstallation command without logging
+            $UninstallCommand = "/x $($7ZipProductCode) /qn"
             
             # Attempt to uninstall 7-Zip
             try {
@@ -45,24 +39,18 @@ if ($7ZipItems) {
 
                 # Check if uninstallation was successful
                 if ($process.ExitCode -eq 0) {
-                    Write-Output "7-Zip version $Version has been uninstalled."
                     exit 0  # Exit with success code
                 } else {
-                    Write-Output "Failed to uninstall 7-Zip. Exit code: $($process.ExitCode)"
-                    Write-Output "Check the log file at $env:TEMP\7Zip_Uninstall.log for more details."
                     exit 1  # Exit with failure code
                 }
             } catch {
-                Write-Output "Failed to start the uninstallation process. Error: $_"
                 exit 1
             }
         } else {
-            Write-Output "No action required. 7-Zip version meets minimum requirement."
             exit 0  # Exit with success code
         }
     }
 } else {
     # If 7-Zip is not found
-    Write-Output "7-Zip is not installed."
-    exit 1  # Exit with failure code
+    exit 0  # Exit with success code
 }
