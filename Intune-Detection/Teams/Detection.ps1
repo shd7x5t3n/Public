fe4ab99ddaaeb7caa50b32188943cb279f538ee1
calvin-quint <138726054+calvin-quint@users.noreply.github.com>
2024-09-22 09:57:24 -0500

    Update Detection.ps1

--- Intune-Detection/Teams/Detection.ps1
+++ Intune-Detection/Teams/Detection.ps1
@@ -1,6 +1,13 @@
-$NewTeams = $null
 $windowsAppsPath = "C:\Program Files\WindowsApps"
 $NewTeamsSearch = "MSTeams_*_x64__*"
-$NewTeams = Get-ChildItem -Path $windowsAppsPath -Directory -Filter $NewTeamsSearch  -ErrorAction SilentlyContinue
-if ($NewTeams ) {Write-Host "New Teams found";exit 0}
-else {Write-Host "New Teams not found";exit 1}
+
+# Search for New Teams installation
+$NewTeams = Get-ChildItem -Path $windowsAppsPath -Directory -Filter $NewTeamsSearch -ErrorAction SilentlyContinue
+
+if ($NewTeams) {
+    Write-Host "New Teams found"
+    exit 0
+} else {
+    Write-Host "New Teams not found"
+    exit 1
+}
